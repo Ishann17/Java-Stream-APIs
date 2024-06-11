0586dc3d5a46b1f02b106c9d0381137caa950653
Ishan <106976566+Ishann17@users.noreply.github.com>
2024-06-11 18:19:26 +0530

    Sort the Employees by their Salary

--- src/Sorting.java
+++ src/Sorting.java
@@ -40,16 +40,29 @@ class Employees{
 }
 public class Sorting {
     public static void main(String[] args) {
-        Employees e1 = new Employees(1, "Rahul", 10000);
+        Employees e1 = new Employees(1, "Rahul", 9000);
         Employees e2 = new Employees(2, "Varun", 2000);
         Employees e3 = new Employees(3, "Gautam", 3000);
+        Employees e4 = new Employees(3, "Gautam", 8000);
+        Employees e5 = new Employees(3, "Gautam", 3000);
+        Employees e6 = new Employees(3, "Gautam", 7000);
+        Employees e7 = new Employees(3, "Gautam", 5000);
 
         List<Employees> elist = new ArrayList<>();
         elist.add(e1);
         elist.add(e2);
         elist.add(e3);
+        elist.add(e4);
+        elist.add(e5);
+        elist.add(e6);
+        elist.add(e7);
 
-        List<Employees> sort = elist.stream().sorted(Comparator.comparingInt(Employees::getSalary)).toList();
-        System.out.println(sort);
+        //Ascending Order
+        List<Employees> sortASC = elist.stream().sorted(Comparator.comparingInt(Employees::getSalary)).toList();
+        System.out.println(sortASC);
+
+        //Descending Order
+        List<Employees> sortDESC = elist.stream().sorted(Comparator.comparingInt(Employees::getSalary).reversed()).toList();
+        System.out.println(sortDESC);
     }
 }
